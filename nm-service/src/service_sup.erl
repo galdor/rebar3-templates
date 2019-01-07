1da96e342cf1ec05ed58c3f5d22e457db1699767
Nicolas Martyanoff <khaelin@gmail.com>
2019-01-07 09:31:11 +0100

    fix templates

--- nm-service/src/service_sup.erl
+++ nm-service/src/service_sup.erl
@@ -1,5 +1,5 @@
 {{=@@ @@=}}
-%% Copyright (c) {{copyright_year}} {{author_name}} <{{author_email}}>.
+%% Copyright (c) @@copyright_year@@ @@author_name@@ <@@author_email@@>.
 %%
 %% Permission to use, copy, modify, and/or distribute this software for any
 %% purpose with or without fee is hereby granted, provided that the above
